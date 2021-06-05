c1309dd7ef7c539c235cee07c05cbfd505f2c78c
Thomas Lourenco <TSLourenco@WPI.edu>
2021-06-05 12:49:13 -0400

    Converted Kinematics.java. Added wheel CalConstants to the Constants.hpp file. Closes issue #5.

--- cpp/src/main/include/Constants.hpp
+++ cpp/src/main/include/Constants.hpp
@@ -2,3 +2,9 @@
 
 #define K_LOOPER_DT 0.01_s
 #define K_LOG_REPORT_RATE 0.02_s
+
+// Wheel constants from CalConstants Java file. Should find new home.
+#define K_DRIVE_WHEEL_TRACK_WIDTH_INCHES 23.5
+#define K_DRIVE_WHEEL_DIAMETER_INCHES 5.0 * 0.9625
+#define K_DRIVE_WHEEL_RADIUS_INCHES K_DRIVE_WHEEL_DIAMETER_INCHES / 2.0
+#define K_TRACK_SCRUB_FACTOR 1.0 // Tune me!
