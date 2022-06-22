6175709597d7852d02180c0b2c20c5aedb14de46
Chris Bonomi <bonomi.c11@gmail.com>
2022-06-21 20:18:27 -0400

    Implement base robot code to run an auto

--- cpp/src/main/include/Robot.hpp
+++ cpp/src/main/include/Robot.hpp
@@ -1,15 +1,20 @@
 #pragma once
 
 #include <string>
-#include <frc/livewindow/LiveWindow.h>
+// #include <frc/livewindow/LiveWindow.h>
 #include <iostream>
 #include <frc/TimedRobot.h>
+#include "frc/Timer.h"
 
 #include "SubsystemManager.hpp"
 #include "utils/Looper/Looper.hpp"
 #include "reporters/NetworkDataType.hpp"
 
+#include "autonomous/AutoModeExecutor.hpp"
+#include "autonomous/modes/TestMode.hpp"
 #include "subsystems/Drive.hpp"
+#include "paths/TrajectoryGenerator.hpp"
+#include "RobotState.hpp"
 
 class Robot : public frc::TimedRobot {
 public:
@@ -23,8 +28,12 @@ public:
     void DisabledInit() override;
     void DisabledPeriodic() override;
 
+    static AutoModeExecutor *mAutoModeExecutor; 
+
 private:
     SubsystemManager* mSubsystemManager;
     Looper mEnabledLooper;
     Looper mDisabledLooper;
+
+    Drive *mDrive;
 };
